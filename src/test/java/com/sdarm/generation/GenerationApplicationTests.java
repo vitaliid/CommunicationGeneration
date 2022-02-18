58674b34350302b9e11e3b75d293eebb48a72bf3
vitalii.dudok <vitalii.dudok@kindgeek.com>
2022-02-18 11:54:43 +0200

    Added skeleton

---
+++ src/test/java/com/sdarm/generation/GenerationApplicationTests.java
@@ -0,0 +1,15 @@
+package com.sdarm.generation;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertNull(null);
+	}
+
+}
