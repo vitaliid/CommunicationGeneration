7ac43c5194eefef8cc717db31a92ebf6913ecd56
vitalii.dudok <vitalii.dudok@kindgeek.com>
2022-02-20 15:39:04 +0200

    Added logs to controller

--- src/main/java/com/sdarm/generation/controller/GenerationController.java
+++ src/main/java/com/sdarm/generation/controller/GenerationController.java
@@ -4,10 +4,12 @@ import com.sdarm.generation.dto.GenerationCreateRequest;
 import com.sdarm.generation.facade.GenerationFacade;
 import com.sdarm.generation.dto.GenerationResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("api/v1/generations")
 @RequiredArgsConstructor
@@ -17,21 +19,25 @@ public class GenerationController {
 
     @GetMapping
     public List<GenerationResponse> getAll() {
+        log.info("Request for getting all generations");
         return generationFacade.getAll();
     }
 
     @GetMapping("latest")
     public GenerationResponse get() {
+        log.info("Request for getting the latest generation");
         return generationFacade.getLatest();
     }
 
     @GetMapping("{id}")
     public GenerationResponse get(@PathVariable Long id) {
+        log.info("Request for getting generation with id {}", id);
         return generationFacade.getById(id);
     }
 
     @PostMapping
     public GenerationResponse create(@RequestBody GenerationCreateRequest generationCreateRequest) {
+        log.info("Request for creating generation");
         return generationFacade.create(generationCreateRequest);
     }
 
--- src/main/java/com/sdarm/generation/controller/ParticipantController.java
+++ src/main/java/com/sdarm/generation/controller/ParticipantController.java
@@ -3,12 +3,14 @@ package com.sdarm.generation.controller;
 import com.sdarm.generation.dto.ParticipantResponse;
 import com.sdarm.generation.facade.ParticipantFacade;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("api/v1/participants")
 @RequiredArgsConstructor
@@ -18,6 +20,7 @@ public class ParticipantController {
 
     @GetMapping
     public List<ParticipantResponse> getAll() {
+        log.info("Request for getting all participants");
         return participantFacade.getAll();
     }
 
